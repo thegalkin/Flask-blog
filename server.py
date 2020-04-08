d1ca848e617b20f95d913d2004429e1956f3c2a7
thegalkin <gozammer@gmail.com>
2020-04-09 01:29:23 +0300

    --- Auto-Git Commit ---

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import sqlite3
-from flask_admin import Admin
+#from flask_admin import Admin
 from flask_basicauth import BasicAuth
 
 
@@ -16,18 +16,22 @@ from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
 
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+#app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
-admin = Admin(app, name='microblog', template_mode='bootstrap3')
+#admin = Admin(app, name='microblog', template_mode='bootstrap3')
 
 app.config['BASIC_AUTH_USERNAME'] = 'admin'
+app.config['BASIC_AUTH_PASSWORD'] = '1234'
+app.config['BASIC_AUTH_USERNAME'] = 'user'
 app.config['BASIC_AUTH_PASSWORD'] = '1234'
 basic_auth = BasicAuth(app)
 @app.route('/')
 @app.route('/index')
 def main():
     return render_template("index.html")
-
+@app.route('/register')
+def register():
+    return render_template("register.html")
 @app.route('/texts/<textName>')
 @app.errorhandler(404)
 def page_not_found(error):
@@ -38,10 +42,10 @@ def texts():
 @basic_auth.required
 def editor():
     return render_template("editor.html")   
-@app.route('/admin')
+"""@app.route('/admin')
 @basic_auth.required
 def admin():
-    return render_template("admin/index.html")
+    return render_template("admin/index.html")"""
 
 
 
