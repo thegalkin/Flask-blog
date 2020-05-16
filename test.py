171638bf1811c4aea50844bbc311450c18fc534b
thegalkin <gozammer@gmail.com>
2020-05-16 14:25:57 +0300

    --- Auto-Git Commit ---

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import sqlite3
 newPostId = 10
 connData = sqlite3.connect("userData.db")
+connData.row_factory = lambda cursor, row: row[0]
 g = connData.cursor()
 login = "io"
-temp = g.execute("SELECT posts FROM `userData` WHERE nick=?;", (login,)).fetchone()[0]
+
+temp = g.execute("SELECT posts FROM `userData` WHERE nick=?;", (login,)).fetchall()
 print(temp)
 temp.append(newPostId)
 
